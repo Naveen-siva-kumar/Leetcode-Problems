d81717b58290d2faad9d7bf71818f69f6429422b
Naveen <69638584+Naveen-siva-kumar@users.noreply.github.com>
2022-08-30 16:06:29 +0530

    Time: 102 ms (54.86%), Space: 40 MB (32.49%) - LeetHub

---
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        
+        int globalMax = nums[0],globalMin = nums[0];
+        int localMax = nums[0],localMin = nums[0];
+        
+        int totalSum =nums[0];
+        
+        for(int i=1;i<nums.size();i++){
+            
+            totalSum+=nums[i];
+            localMax=std::max(nums[i],localMax+nums[i]);
+            globalMax = std::max(globalMax,localMax);
+            
+            localMin = std::min(nums[i],localMin+nums[i]);
+            globalMin = std::min(globalMin,localMin);
+                
+        }
+        //cout<<"localMin "<<localMin<<"GlobalMin "<<globalMin<<endl;
+        //cout<<"localMax "<<localMax<<"GlobalMax "<<globalMax<<endl;
+        int ans = std::max(globalMax,totalSum-globalMin);
+        //cout<<ans<<endl;
+        return ans>0 ? ans : globalMax;
+    }
+};
