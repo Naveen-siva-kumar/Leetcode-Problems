14675ebe669671f74f90950e0552bf0961220b90
Naveen <69638584+Naveen-siva-kumar@users.noreply.github.com>
2022-09-20 14:23:27 +0530

    Time: 197 ms (42.74%), Space: 45.5 MB (68.41%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -6,31 +6,23 @@ public:
         int tempSwap =0;
         k=nums.size()-k+1;
         int l = left;int r = right;
-        
         return threeWayPartition(nums,k,right,left,tempSwap,l,r);   
     }
     public:
     int threeWayPartition(vector<int>& nums,int& k,int& right,int& left,int& tempSwap,int& l,int& r){
-        
         if(left==right){return nums[left];}
-        
         srand((unsigned) time(NULL));
         int pivotIndex = (rand() % (right-left+1)) + left;
-        
         swap(nums[pivotIndex],nums[right],tempSwap); //Now my last ele is pivot
         int pivot = nums[right];
         cout<<"pivot :"<<pivot<<endl;
-        
         l=left-1;
         for(r=left;r<right;r++){
             if(nums[r]<pivot){
                 l++;
                 swap(nums[l],nums[r],tempSwap);
-                
             }
-            
         }swap(nums[l+1],nums[right],tempSwap);
-        
         if(l+1 == k-1){
             return nums[l+1];
         }
@@ -38,13 +30,10 @@ public:
             right = l;
             return threeWayPartition(nums,k,right,left,tempSwap,l,r);
         }
-        else{// if(l+1 < k){
+        else{
             left = l+2;
-            //k=k-(left);
             return threeWayPartition(nums,k,right,left,tempSwap,l,r);
         }
-        
-        
     }
     public:
     void swap(int &a,int &b,int &tempSwap){
